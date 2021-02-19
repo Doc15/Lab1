fd39f2db778e3631171f1704f1f3bc48a4c53ca8
Doc15 <54259761+Doc15@users.noreply.github.com>
2021-02-19 17:37:45 +0700

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query = "select title, title_t, title_c from product join type on id_t=id_type join category on id_cat = id_category";
+            MySqlConnection conn = Class2.GetDbConnection();
+            MySqlCommand cmDB = new MySqlCommand(query, conn);
+            cmDB.CommandTimeout = 60;
+            MySqlDataReader rd; 
+            try
+            {
+                conn.Open();
+                rd = cmDB.ExecuteReader();
+                if (rd.HasRows){
+                    while(rd.Read())
+                        {
+                        string[] row = { rd.GetString(0), rd.GetString(1), rd.GetString(2) };
+                        var listViewItem = new ListViewItem(row);
+                            listView1.Items.Add(listViewItem);
+                    }
+                }    
+                conn.Close();
+                listView1.AutoResizeColumns(ColumnHeaderAutoResizeStyle.ColumnContent);
+                MessageBox.Show("Подключение к БД прошло успешно ");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Ошибка подключения к БД");
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
